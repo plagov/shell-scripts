5b74a37b93f8c5ea267acd74b2cd582e62ee407c
Vitalii Plagov <vitalii.plagov@gmail.com>
2017-01-06 23:17:11 +0200

    Script automaticaly deletes files from trash older X days. Script is run by crontab

---
+++ deleteTrashFiles.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#This script deletes all files from Trash folder that are older than 5 days
+#Scripts is running every day at 21:00 by crontab
+#0 21 * * * deleteTrashFiles.sh
+find ~/.local/share/Trash/files/ -mtime +5 -delete
