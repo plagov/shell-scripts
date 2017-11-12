4cb0217bfebbec2c5e2e11b4b486cd5fc4d535af
Vitalii Plagov <vitalii.plagov@gmail.com>
2017-11-12 22:37:24 +0200

    Change mysql script to make dumb from docker container

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -e
 
 user="root"
-host="127.0.0.1"
 db_name="music"
 backup_path="/home/vplagov/.mysql-backup"
 
-mysqldump --user="${user}" --host="${host}" "${db_name}" > "${backup_path}"/"${db_name}".sql
+docker exec music-sql /usr/bin/mysqldump -u "${user}" music > "${backup_path}"/"${db_name}".sql
 
 cd "${backup_path}"
 
