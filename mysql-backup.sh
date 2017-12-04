2dd4709400ac0de00403aa0702a74a8602c74e6b
Vitalii Plagov <vitalii.plagov@gmail.com>
2017-12-04 11:13:58 +0200

    Add a check if docker container is runniing
    If docker container is not running, script will exit. This will prevent
    a mysqldump execution that results in an empty dump file if container is not
    running.

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -4,7 +4,16 @@ user="root"
 db_name="music"
 backup_path="/home/vplagov/.mysql-backup"
 
-docker exec music-sql /usr/bin/mysqldump -u "${user}" music > "${backup_path}"/"${db_name}".sql
+# container variables
+container_name="music-sql"
+running_container=$(docker inspect -f '{{.State.Running}}' "${container_name}")
+
+# check if container is running
+if [ "${running_container}" = "false" ]; then
+    exit 1
+fi
+
+docker exec "${container_name}" /usr/bin/mysqldump -u "${user}" "${db_name}" > "${backup_path}"/"${db_name}".sql
 
 cd "${backup_path}"
 
